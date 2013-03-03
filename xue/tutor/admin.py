ad6f1a4fe367b42fb597aaee4133ae321f0a04ca
Wang Xuerui <idontknw.wang@gmail.com>
2013-03-04 03:40:46 +0800

    改进管理界面下模型呈现
    * 让导师项目的数据分栏显示并可过滤
    * 赋予所有模型汉语名称

--- xue/tutor/admin.py
+++ xue/tutor/admin.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals, division
+
 from xue.tutor.models import *
 from django.contrib import admin
 
-admin.site.register(StudentApplication)
-admin.site.register(TutorProject)
+
+class TutorProjectAdmin(admin.ModelAdmin):
+    list_display = (
+            'get_teacher_name',
+            'teacher',
+            'name',
+            'year',
+            )
+
+    list_filters = (
+            'get_teacher_name',
+            'year',
+            )
+
+
+admin.site.register(TutorProject, TutorProjectAdmin)
 admin.site.register(StudentProject)
+admin.site.register(StudentApplication)
 
 
 # vim:ai:et:ts=4:sw=4:sts=4:fenc=utf-8
