c7e2d740e0367d86ca10d79995d047faac0d402a
Wang Xuerui <idontknw.wang@gmail.com>
2013-03-04 04:00:06 +0800

    改进管理界面下模型呈现 #2
    * 修正一个参数拼写错误导致的过滤器不显示
    * 让学生申请分栏显示并可过滤

--- xue/tutor/admin.py
+++ xue/tutor/admin.py
@@ -14,15 +14,35 @@ class TutorProjectAdmin(admin.ModelAdmin):
             'year',
             )
 
-    list_filters = (
+    list_filter = (
             'get_teacher_name',
             'year',
             )
 
 
+class StudentApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+            'student',
+            'get_student_realname',
+            'get_student_year',
+            'get_student_major',
+            'get_student_klass',
+            'get_student_political',
+            'status',
+            )
+
+    list_filter = (
+            'get_student_year',
+            'get_student_major',
+            'get_student_klass',
+            'get_student_political',
+            'status',
+            )
+
+
 admin.site.register(TutorProject, TutorProjectAdmin)
 admin.site.register(StudentProject)
-admin.site.register(StudentApplication)
+admin.site.register(StudentApplication, StudentApplicationAdmin)
 
 
 # vim:ai:et:ts=4:sw=4:sts=4:fenc=utf-8
